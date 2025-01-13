0b01901c5fc5b476ca79987b98a762c56c91b4a8
Mikołaj Bogacki <mikolajbogacki13@gmail.com>
2025-01-13 23:16:13 +0100

    Added statuses enumeration for Tasks

--- Levelling/tasks/models.py
+++ Levelling/tasks/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+
+
+class Status(Enum):
+    TODO = 1
+    IN_PROGRESS = 2
+    DONE = 3
 
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    status = models.IntegerField(choices=[(tag.value, tag.name) for tag in Status])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
